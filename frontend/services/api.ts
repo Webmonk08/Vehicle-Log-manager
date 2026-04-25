ba48cf8023533ddfec3723d09ae3cd0abfc0e30e
webmonk08 <nikeshnskm@gmail.com>
2026-04-25 13:15:21 +0530

    feat: initialize project configuration, environment variables, dependencies, and API service layer

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -7,7 +7,7 @@ import {
 } from '@/types';
 
 // Change this to your backend URL
-const BASE_URL = 'http://10.177.25.42:8000/api/v1';
+const BASE_URL = 'https://vr-app-backend.onrender.com/api/v1';
 
 const api = axios.create({
   baseURL: BASE_URL,
